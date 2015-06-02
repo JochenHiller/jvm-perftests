29fb7e770af47e81e6a20e28431c68e0fe741615
Jochen Hiller <jo.hiller@googlemail.com>
2015-06-02 23:50:12 +0200

    Refactored SPECjvm2008 scripts/results

--- SPECjvm2008/install.sh
+++ SPECjvm2008/install.sh
@@ -2,12 +2,25 @@
 
 echo "Installing SPECjvm2008..."
 
-curl -s -o SPECjvm2008_1_01_setup.jar http://spec.cs.miami.edu/downloads/osg/java/SPECjvm2008_1_01_setup.jar
+if [ -z $JAVA_HOME ] ; then
+  echo "Set JAVA_HOME first to unpack SPECjvm2008!"
+  exit 1
+fi
+
+# check if curl can be called, some embedded systems only use wget
+curl 2>/dev/null
+rc=$?
+if [ $rc -eq 2 ] ; then
+  curl -s -o SPECjvm2008_1_01_setup.jar http://spec.cs.miami.edu/downloads/osg/java/SPECjvm2008_1_01_setup.jar
+else
+  wget --no-check-certificate --quiet -O SPECjvm2008_1_01_setup.jar http://spec.cs.miami.edu/downloads/osg/java/SPECjvm2008_1_01_setup.jar
+fi
+
 export SPECJVM_HOME=`pwd`/specjvm2008
-java -jar SPECjvm2008_1_01_setup.jar -DUSER_INSTALL_DIR=$SPECJVM_HOME -i silent
+$JAVA_HOME/bin/java -jar SPECjvm2008_1_01_setup.jar -DUSER_INSTALL_DIR=$SPECJVM_HOME -i silent
 # fix wrong script file
 dos2unix $SPECJVM_HOME/report.sh
 # make a trial run
 cd specjvm2008
-java -jar SPECjvm2008.jar -wt 5s -it 5s -bt 2 compress
+$JAVA_HOME/bin/java -jar SPECjvm2008.jar -wt 5s -it 5s -bt 2 compress
 cd ..
--- SPECjvm2008/run.sh
+++ SPECjvm2008/run.sh
@@ -2,6 +2,11 @@
 
 echo "Running SPECjvm2008 for Java $JAVA_HOME"
 
+if [ -z $JAVA_HOME ] ; then
+  echo "Set JAVA_HOME first to unpack SPECjvm2008!"
+  exit 1
+fi
+
 cd specjvm2008
 
 # make a dry run for fast testing
@@ -12,13 +17,7 @@ cd specjvm2008
 
 # run only tests which run in JavaSE 8
 # give more heap memory as "derby" test will fail otherwise on Raspberry Pi
-java -Xmx600m -jar SPECjvm2008.jar startup.helloworld startup.compiler.compiler startup.compress startup.crypto.aes startup.crypto.rsa startup.crypto.signverify startup.mpegaudio startup.scimark.fft startup.scimark.lu startup.scimark.monte_carlo startup.scimark.sor startup.scimark.sparse startup.serial startup.sunflow startup.xml.transform startup.xml.validation compiler.compiler compress crypto.aes crypto.rsa crypto.signverify derby mpegaudio scimark.fft.large scimark.lu.large scimark.sor.large scimark.sparse.large scimark.fft.small scimark.lu.small scimark.sor.small scimark.sparse.small scimark.monte_carlo serial sunflow xml.transform xml.validation
+$JAVA_HOME/bin/java -Xmx600m -jar SPECjvm2008.jar \
+  startup.helloworld startup.compiler.compiler startup.compress startup.crypto.aes startup.crypto.rsa startup.crypto.signverify startup.mpegaudio startup.scimark.fft startup.scimark.lu startup.scimark.monte_carlo startup.scimark.sor startup.scimark.sparse startup.serial startup.sunflow startup.xml.transform startup.xml.validation compiler.compiler compress crypto.aes crypto.rsa crypto.signverify derby mpegaudio scimark.fft.large scimark.lu.large scimark.sor.large scimark.sparse.large scimark.fft.small scimark.lu.small scimark.sor.small scimark.sparse.small scimark.monte_carlo serial sunflow xml.transform xml.validation
 
 cd ..
-
-
-exit 1
-tests on raspi
-java -Xmx400m -jar SPECjvm2008.jar derby mpegaudio scimark.fft.large scimark.lu.large scimark.sor.large scimark.sparse.large scimark.fft.small scimark.lu.small scimark.sor.small scimark.sparse.small scimark.monte_carlo serial sunflow xml.transform xml.validation
-java -Xmx600m -jar SPECjvm2008.jar scimark.fft.large
-java -Xmx600m -jar SPECjvm2008.jar scimark.lu.large scimark.sor.large scimark.sparse.large scimark.fft.small scimark.lu.small scimark.sor.small scimark.sparse.small scimark.monte_carlo serial sunflow xml.transform xml.validation
